cd2889eb1aa4293d0ff2b938b19e7f270d59f4c1
황여원 <yeooness@naver.com>
2022-10-13 17:52:09 +0900

    ing

---
+++ django/1013/accounts/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "password1", "password2", "email")
+        labels = {
+            "username": "닉네임",
+            "password1": "비밀번호",
+            "password2": "비밀번호 확인",
+            "email": "이메일",
+        }
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ("first_name", "last_name", "email")
