b6ab75143bf9e47034be6f81b2bb1285b32ce00f
황여원 <yeooness@naver.com>
2022-10-18 19:11:48 +0900

    댓글

---
+++ django/1018/articles/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+"""
+게시판 기능
+- 제목(20글자이내)
+- 내용(긴 글)
+- 글 작성시간 / 글 수정시간(자동으로 기록, 날짜/시간)
+"""
+
+# 1. 모델 설계 (=DB 스키마 설계)
+# Article 은 models 에 있는 Model 을 상속 받음
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(400, 300)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
